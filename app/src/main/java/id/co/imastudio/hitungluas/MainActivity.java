ccc803d23a1717a1cccb8cae77d9c399068b58b5
ima2 <mursitaffandi@gmail.com>
2016-10-07 02:31:31 +0700

    succes adding validation field for length and width

---
+++ app/src/main/java/id/co/imastudio/hitungluas/MainActivity.java
@@ -0,0 +1,50 @@
+package id.co.imastudio.hitungluas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText edtPanjang, edtLebar;
+    private Button btnHitung;
+    private TextView txtLuas;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("Hitung Luas Persegi");
+
+        edtLebar = (EditText) findViewById(R.id.edt_lebar);
+        edtPanjang = (EditText) findViewById(R.id.edt_panjang);
+        btnHitung = (Button) findViewById(R.id.btn_hitung);
+        txtLuas = (TextView) findViewById(R.id.txt_luas);
+
+        btnHitung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                edtLebar.setError(null);
+                edtPanjang.setError(null);
+                String panjang = edtPanjang.getText().toString().trim();
+                String lebar = edtLebar.getText().toString().trim();
+                if (panjang.matches("")) {
+                    edtPanjang.setError("This field for length");
+                    edtPanjang.requestFocus();
+                } else if (lebar.matches("")) {
+                    edtLebar.setError("This field for width");
+                    edtLebar.requestFocus();
+                } else {
+
+                    double p = Double.parseDouble(panjang);
+                    double l = Double.parseDouble(lebar);
+
+                    double luas = p * l;
+                    txtLuas.setText("Luas : " + luas);
+                }
+            }
+        });
+    }
+}
